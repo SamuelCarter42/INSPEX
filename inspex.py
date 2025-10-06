8b64160e049bd972cff6dab139f99847a0fa0966
SamuelCarter42 <w23014130@northumbria.ac.uk>
2025-10-06 15:09:26 +0100

    Function drop off (#30)
    * Update inspex.py
    
    added loading bar window for fit progress
    
    * change function defs
    
    change power law functions to feature a drop off to low energy end, using erf function
    
    * Update inspex.py
    
    * reset init params for erfs
    
    * initial guess changes
    
    * Delete inspex.cpython-311.pyc
    
    * Create .gitignore

--- inspex.py
+++ inspex.py
@@ -417,11 +417,8 @@ def gauss_func(x,gauss_amp,gauss_centre,sigma): #defines a gaussian function tha
     y_gauss=gauss_amp*np.exp((-(x-gauss_centre)**2)/(2*sigma**2))
     return y_gauss
 
-<<<<<<< HEAD
-def power_func(x,A_sing,B_sing,dx_sing,x0_sing): #defines a simgle power law that can be added
-=======
 def power_func(x,A_sing,B_sing,x0_sing,dx_sing): #defines a simgle power law that can be added
->>>>>>> origin/function-drop-off
+
     x=np.array(x)
     xlo_sing=(erf(((x-x0_sing)/dx_sing))+1)/2#below x0
     y_pow=xlo_sing*(A_sing*(x**B_sing))
@@ -1940,11 +1937,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             init['A2']=1e5
             init['B2']=-2    
             init['x0_bpl']=1
-<<<<<<< HEAD
-            init['dx_bpl']=1    
-=======
             init['dx_bpl']=0.1    
->>>>>>> origin/function-drop-off
+
 
     
             global vary#define global if vary values for the params of the function
@@ -1965,11 +1959,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             maxval['A2']=None
             maxval['B2']=0            
             maxval['x0_bpl']=10
-<<<<<<< HEAD
-            maxval['dx_bpl']=10
-=======
             maxval['dx_bpl']=1
->>>>>>> origin/function-drop-off
+
 
             
             global minval##define global minimum values for the params of the function
@@ -1979,13 +1970,9 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             minval['B']=-10
             minval['A2']=0
             minval['B2']=-10            
-<<<<<<< HEAD
-            minval['x0_bpl']=0.1
-            minval['dx_bpl']=0.1
-=======
             minval['x0_bpl']=-1
             minval['dx_bpl']=0.01
->>>>>>> origin/function-drop-off
+
 
             global frame_bpl#defining gui section to handle bpl param options
             frame_bpl=tk.Frame(master=frame_params)
@@ -2081,11 +2068,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             init['A_sing']=1e9
             init['B_sing']=-1
             init['x0_sing']=1
-<<<<<<< HEAD
-            init['dx_sing']=1    
-=======
             init['dx_sing']=0.1    
->>>>>>> origin/function-drop-off
+
             
             global vary#define global if vary values for the params of the function
             vary['A_sing']=True
@@ -2096,23 +2080,16 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             global minval##define global minimum values for the params of the function
             minval['A_sing']=0
             minval['B_sing']=None
-<<<<<<< HEAD
-            minval['x0_sing']=0.1
-            minval['dx_sing']=0.1
-=======
             minval['x0_sing']=-1
             minval['dx_sing']=0.01
->>>>>>> origin/function-drop-off
+
             
             global maxval##define global maximum values for the params of the function
             maxval['A_sing']=None
             maxval['B_sing']=0
             maxval['x0_sing']=10
-<<<<<<< HEAD
-            maxval['dx_sing']=10
-=======
             maxval['dx_sing']=1
->>>>>>> origin/function-drop-off
+
             
             global frame_power
             frame_power=tk.Frame(master=frame_params)
@@ -2375,11 +2352,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             init['A3_tpl']=1e5
             init['B3_tpl']=-2   
             init['x0_tpl']=1
-<<<<<<< HEAD
-            init['dx_tpl']=1  
-=======
             init['dx_tpl']=0.1  
->>>>>>> origin/function-drop-off
+
     
             global vary#define global if vary values for the params of the function
 
@@ -2405,11 +2379,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             maxval['A3_tpl']=None
             maxval['B3_tpl']=0           
             maxval['x0_tpl']=10
-<<<<<<< HEAD
-            maxval['dx_tpl']=10
-=======
             maxval['dx_tpl']=1
->>>>>>> origin/function-drop-off
+
 
             
             global minval##define global minimum values for the params of the function
@@ -2422,13 +2393,9 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             minval['B2_tpl']=-10            
             minval['A3_tpl']=0
             minval['B3_tpl']=-10            
-<<<<<<< HEAD
-            minval['x0_tpl']=0.1
-            minval['dx_tpl']=0.1
-=======
             minval['x0_tpl']=-1
             minval['dx_tpl']=0.01
->>>>>>> origin/function-drop-off
+
 
             global frame_tpl#defining gui section to handle tpl param options
             frame_tpl=tk.Frame(master=frame_params)
@@ -2496,11 +2463,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             init['A4_qpl']=1e9
             init['B4_qpl']=-2 
             init['x0_qpl']=1
-<<<<<<< HEAD
-            init['dx_qpl']=1    
-=======
             init['dx_qpl']=0.1    
->>>>>>> origin/function-drop-off
+
     
             global vary#define global if vary values for the params of the function
 
@@ -2532,11 +2496,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             maxval['A4_qpl']=None
             maxval['B4_qpl']=0           
             maxval['x0_qpl']=10
-<<<<<<< HEAD
-            maxval['dx_qpl']=10
-=======
             maxval['dx_qpl']=1
->>>>>>> origin/function-drop-off
+
             
             global minval##define global minimum values for the params of the function
 
@@ -2551,13 +2512,9 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             minval['B3_qpl']=-10
             minval['A4_qpl']=0
             minval['B4_qpl']=-10            
-<<<<<<< HEAD
-            minval['x0_qpl']=0.1
-            minval['dx_qpl']=0.1
-=======
             minval['x0_qpl']=-1.1
             minval['dx_qpl']=0.01
->>>>>>> origin/function-drop-off
+
             
             global frame_qpl#defining gui section to handle qpl param options
             frame_qpl=tk.Frame(master=frame_params)
@@ -2639,11 +2596,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             init['A5_5pl']=1e9
             init['B5_5pl']=-2 
             init['x0_5pl']=1
-<<<<<<< HEAD
-            init['dx_5pl']=1    
-=======
             init['dx_5pl']=0.1    
->>>>>>> origin/function-drop-off
+
     
             global vary#define global if vary values for the params of the function
 
@@ -2681,11 +2635,8 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             maxval['A5_5pl']=None
             maxval['B5_5pl']=0 
             maxval['x0_5pl']=10
-<<<<<<< HEAD
-            maxval['dx_5pl']=10
-=======
             maxval['dx_5pl']=1
->>>>>>> origin/function-drop-off
+
             
             global minval##define global minimum values for the params of the function
 
@@ -2703,13 +2654,9 @@ def build_fit_window(x_data, y_data, uncert, date, inst, spec_type):
             minval['B4_5pl']=-10
             minval['A5_5pl']=0
             minval['B5_5pl']=-10            
-<<<<<<< HEAD
-            minval['x0_5pl']=0.1
-            minval['dx_5pl']=0.1
-=======
             minval['x0_5pl']=-1
             minval['dx_5pl']=0.01
->>>>>>> origin/function-drop-off
+
 
             global frame_quint_pl#defining gui section to handle quint_pl param options
             frame_quint_pl=tk.Frame(master=frame_params)
